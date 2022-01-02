8f129fa71a3be3adc4156f7d31017cee35bd90d3
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-02 09:48:22 +0530

    made variables and functions private

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,16 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex += 1;
+      _questionIndex += 1;
     });
   }
 
@@ -36,10 +36,10 @@ class MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: [
-          Text(questions[questionIndex]),
-          RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion),
-          RaisedButton(child: Text("Answer 2"), onPressed: answerQuestion),
-          RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion)
+          Text(questions[_questionIndex]),
+          RaisedButton(child: Text("Answer 1"), onPressed: _answerQuestion),
+          RaisedButton(child: Text("Answer 2"), onPressed: _answerQuestion),
+          RaisedButton(child: Text("Answer 3"), onPressed: _answerQuestion)
         ],
       ),
     ));

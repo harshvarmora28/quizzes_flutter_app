fed92e67b0b3abb8c7842dd651c4813be44999c3
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-03 15:39:35 +0530

    working on displaying options and answers

---
+++ lib/answer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  // const Answer({ Key? key }) : super(key: key);
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Text("Answer 1"), onPressed: selectHandler),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
 
+import './question.dart';
+import './answer.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -36,10 +39,10 @@ class _MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: [
-          Text(questions[_questionIndex]),
-          RaisedButton(child: Text("Answer 1"), onPressed: _answerQuestion),
-          RaisedButton(child: Text("Answer 2"), onPressed: _answerQuestion),
-          RaisedButton(child: Text("Answer 3"), onPressed: _answerQuestion)
+          Question(questions[_questionIndex]),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion)
         ],
       ),
     ));

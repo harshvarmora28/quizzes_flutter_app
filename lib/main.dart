ddc5cad28601dadd9cfc6674ce4dab4242574381
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-04 21:34:16 +0530

    displaying the personality score based on the options selected by the user

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class _MyAppState extends State<MyApp> {
               questionIndex: _questionIndex,
               questions: _questions,
             )
-          : Result(),
+          : Result(_totalScore),
     ));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if(resultScore <= 26){
+      resultText = "You are not so Unique!";
+    }
+    else if(resultScore <= 28){
+      resultText = "Pretty Likeable!";
+    }
+    else if(resultScore <= 30){
+      resultText = "You are awesome and innocent!";
+    }
+    else{
+      resultText = "You are so Strange?!";
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You did it!"),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+        ),
     );
   }
 }

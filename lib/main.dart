ab7e04d4d7a3ec35b8293018562104a591eaf817
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-04 14:00:47 +0530

    added some basic checks

--- lib/main.dart
+++ lib/main.dart
@@ -19,38 +19,43 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      "questionText": "What's your favorite color?",
+      "answers": ["Black", "Blue", "Peacock", "Green"]
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answers": ["Monkey", "Lion", "Zebra", "Elephant"]
+    },
+    {
+      "questionText": "Who's your favorite instructor?",
+      "answers": ["Harry", "Max", "Carry", "Garry"]
+    }
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIndex += 1;
     });
+
+    if (_questionIndex < questions.length) {
+      print("We have more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        "questionText": "What's your favorite color?",
-        "answers": ["Black", "Blue", "Peacock", "Green"]
-      },
-      {
-        "questionText": "What's your favorite animal?",
-        "answers": ["Monkey", "Lion", "Zebra", "Elephant"]
-      },
-      {
-        "questionText": "Who's your favorite instructor?",
-        "answers": ["Harry", "Max", "Carry", "Garry"]
-      }
-    ];
-
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text("My First App"),
+        title: Text("Quizzes"),
       ),
       body: Column(
         children: [
           Question(questions[_questionIndex]["questionText"]),
-          ...(questions[_questionIndex]["answers"] as List<String>).map((answer) {
+          ...(questions[_questionIndex]["answers"] as List<String>)
+              .map((answer) {
             return Answer(_answerQuestion, answer);
           }).toList()
         ],
